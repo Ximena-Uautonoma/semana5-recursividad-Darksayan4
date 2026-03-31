711ba3587c3fc4a76f5a81c21ce14ccb746f773e
AMuñozMelendez <121844085+Darksayan4@users.noreply.github.com>
2026-03-31 15:05:40 -0300

    Hola profe

--- Semana5/ejercicio1.py
+++ Semana5/ejercicio1.py
@@ -10,8 +10,8 @@ def contar_ciclo(n):
     """
     Retorna una lista con los números desde 1 hasta n usando iteración.
     """
-    # Escriba aquí su solución y borre la palabra pass de acontinuación
-    pass
+    numeros=[]
+    for i range(1,n+1):
 
 
 def contar_recursivo(n):
